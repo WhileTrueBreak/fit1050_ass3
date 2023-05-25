101916067fd8aaf3b6b6a61af21ac394e820fa9f
Jeffrey <jeff.jc2003@gmail.com>
2023-05-25 19:58:54 +1000

    fixes

--- js/dropdown.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('mousemove', () => {
-    document.querySelectorAll('.nav-dropdown').forEach(
-        e => e.querySelectorAll('.nav-drop')[0].style.display=e.matches(':hover')?'block':'none'
-    );
-});
